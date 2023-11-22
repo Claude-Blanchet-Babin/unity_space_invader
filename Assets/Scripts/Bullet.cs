3a050062c9989c7d2a791dcaeda6e004aac2faaa
Claude-Blanchet-Babin <118370226+Claude-Blanchet-Babin@users.noreply.github.com>
2023-11-22 10:20:05 +0100

    Update Bullet.cs
    ajout commentaire

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,32 +4,34 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public Rigidbody2D monRigidBody;
+    // déclaration des variables
+    public Rigidbody2D myRigidBody;
     public float speed;
     public GameObject lootKill;
     public GameObject explosion;
-
-
     public PlayerManager myPlayer;
-
-    //public enemy_travel infoEnemy;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        monRigidBody.velocity = Vector3.up*speed;
+        // définir la vitesse de déplacement du projectile
+        myRigidBody.velocity = Vector3.up*speed;
 
         myPlayer = FindObjectOfType<PlayerManager>();
     }
 
+    // Détectetion d'une collision entre la balle et un ennemi
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //collision.gameObject.GetComponent<enemy_travel>().numberEnemy--;
+        // destruction de l'ennemi
         Destroy(collision.gameObject);
+        // destruction de la balle
         Destroy(gameObject);
+        // faire apparaitre le loot
         Instantiate(lootKill, collision.transform.position, collision.transform.rotation);
+        // faire apparaitre un effet visuel lors de la destruction de l'ennemi
         Instantiate(explosion, collision.transform.position, collision.transform.rotation);
+        // augmentation du score du joueur
         myPlayer.score ++;
 
     }
